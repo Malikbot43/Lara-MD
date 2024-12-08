ed9702cdd033bf442723c4f07935f4085c891858
Malikbot43 <alabiramota2000@gmail.com>
2024-12-08 13:28:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "HugxTSaI#mgg7wxdZ9S40sDnp1E2eaiy2ibVfkjMogtg1o0tDurQ", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
